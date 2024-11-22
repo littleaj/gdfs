65bc310c7ae2598e29bf6b10d21afc3af3181633
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-22 17:34:43 -0500

    add basePath arg for list api call

--- src/model/DriveOperations.ts
+++ src/model/DriveOperations.ts
@@ -1,8 +1,8 @@
-import OperationResult from "./OperationResult";
+import { OperationResult } from "./OperationUtils";
 
 export default interface DriveOperations {
   // TODO clean up these types with the actual type heirarchies
-  list<FileTreeDTO>(): OperationResult<FileTreeDTO>;
+  list<FileTreeDTO>(basePath?: string): OperationResult<FileTreeDTO>;
   delete<FileDescriptionDTO>(file: FileDescriptionDTO): OperationResult;
   download<FileDescriptionDTO>(file: FileDescriptionDTO): OperationResult<FileDescriptionDTO>;
   upload<FileDescriptionDTO>(file: FileDescriptionDTO, to: FileDescriptionDTO): OperationResult<FileDescriptionDTO>;
