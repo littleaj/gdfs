2ac6dbf2cb7f4777b2395a975932be3f56aca62c
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 14:59:39 -0500

    expanded util types

--- src/model/UtilityTypes.ts
+++ src/model/UtilityTypes.ts
@@ -1,1 +1,6 @@
 export type PlainFunction<P extends any[] = any[], R = any> = (...args: P) => R;
+
+export type Maybe<T> = T | undefined;
+
+export type DefinedFunction<P extends any[] = any[], R = any> = R extends undefined ? never : PlainFunction<P, R>;
+export type TotalFunction<P extends any[] = any[], R = any> = R extends undefined | null ? never : PlainFunction<P, R>;
