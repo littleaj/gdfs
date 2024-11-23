32e89aa2468832d6e25a3b80d4c85909b58768e0
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 02:00:12 -0500

    add GAPI client prop to ApiManager implementation

--- src/controllers/GoogleApiManager.ts
+++ src/controllers/GoogleApiManager.ts
@@ -2,6 +2,10 @@ import { OperationResult } from "../model/Operation";
 import ApiManager from "./ApiManager";
 
 export default class GoogleApiManager implements ApiManager {
+  #client: GAPI;
+  constructor(client: GAPI) {
+    this.#client = client;
+  }
   list<FileTreeDTO>(basePath?: string): OperationResult<FileTreeDTO> {
     throw new Error("Method not implemented.");
   }
@@ -14,5 +18,6 @@ export default class GoogleApiManager implements ApiManager {
   upload<FileDescriptionDTO>(file: FileDescriptionDTO, to: FileDescriptionDTO): OperationResult<FileDescriptionDTO> {
     throw new Error("Method not implemented.");
   }
+ 
 
 }
