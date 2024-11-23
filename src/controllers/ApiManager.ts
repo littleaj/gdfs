aaa479b363708f9bd31a7641636ba95ed9ce8f45
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-22 22:44:16 -0500

    turn *Manager into interfaces

--- src/controllers/ApiManager.ts
+++ src/controllers/ApiManager.ts
@@ -1,19 +1,4 @@
 import DriveOperations from "../model/DriveOperations";
-import { OperationResult } from "../model/OperationUtils";
 
-export default class ApiManager implements DriveOperations {
-
-  // TODO verify how paths are constructed in Google Drive API
-  list<FileTreeDTO>(basePath?: string): OperationResult<FileTreeDTO> {
-    throw new Error("Method not implemented.");
-  }
-  delete<FileDescriptionDTO>(file: FileDescriptionDTO): OperationResult {
-    throw new Error("Method not implemented.");
-  }
-  download<FileDescriptionDTO>(file: FileDescriptionDTO): OperationResult<FileDescriptionDTO> {
-    throw new Error("Method not implemented.");
-  }
-  upload<FileDescriptionDTO>(file: FileDescriptionDTO, to: FileDescriptionDTO): OperationResult<FileDescriptionDTO> {
-    throw new Error("Method not implemented.");
-  }
+export default interface ApiManager extends DriveOperations {
 }
--- src/controllers/AuthManager.ts
+++ src/controllers/AuthManager.ts
@@ -1,17 +1,7 @@
+import ApiManager from "./ApiManager";
 
-export default class AuthManager {
-
-  login() {
-    // IMPLEMENT
-  }
-
-  logout() {
-    // IMPLEMENT
-  }
-
-
-  registerApiManager() {
-    // IMPLEMENT this will handle ensuring the ApiManager can access auth data and knows if it's logged in or not.
-  }
-
+export default interface AuthManager {
+  loggedIn: boolean;
+  login(): Promise<void>;
+  logout(): Promise<void>;
 }
