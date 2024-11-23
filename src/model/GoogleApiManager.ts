b37fa0a97e353f438d3945568a68ab8418f27ba5
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-22 22:44:40 -0500

    remove unused abstract class

--- src/model/GoogleApiManager.ts
+++
@@ -1,8 +0,0 @@
-import GoogleApiConfig from "./GoogleApiConfig";
-
-export default abstract class GoogleApiManager {
-  protected config: GoogleApiConfig;
-  constructor(config: GoogleApiConfig) {
-    this.config = config;
-  }
-}
