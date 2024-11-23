f28908325f3be25fd966b7143182aed54d89e47d
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 01:59:27 -0500

    add more google api types and make them visible

---
+++ src/googleapis.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="gapi" />
+/// <reference types="gapi.client" />
+/// <reference types="google.accounts" />
+/// <reference types="gsi" />
+
+type GIS = typeof google.accounts.oauth2
+type GAPI = typeof gapi;
