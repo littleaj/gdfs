e96eaf38bbbf6cd6fc11a8070ac9b497fc9eac0f
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 15:02:19 -0500

    fix naming again

--- src/services/CommandDispatch.ts
+++ src/services/CommandDispatch.ts
@@ -1,13 +1,13 @@
 import { createObservableDelegate, ObservableOperation } from "../model/operations/Observable";
 import { Operation } from "../model/operations/Operation";
 
-type AppendedOperation<T extends object, K extends string, F extends Operation> = T & { [key in K]: F };
-type AppControllerBuilder<T extends Record<string, ObservableOperation> = {}> = {
-  withCommand<Cmd extends string, Op extends Operation>(name: Cmd, op: Op): AppControllerBuilder<AppendedOperation<T, Cmd, Op>>;
+type AppendedCommand<T extends object, K extends string, F extends Operation> = T & { [key in K]: F };
+type CommandDispatchBuilder<T extends Record<string, ObservableOperation> = {}> = {
+  withCommand<Cmd extends string, Op extends Operation>(name: Cmd, op: Op): CommandDispatchBuilder<AppendedCommand<T, Cmd, Op>>;
   create(): T;
 }
 
-export function buildAppController(): AppControllerBuilder {
+export function buildCommandDispatch(): CommandDispatchBuilder {
   const result: Record<string, ObservableOperation> = {};
   const builder = {
     withCommand(name: string, op: Operation) {
@@ -18,7 +18,7 @@ export function buildAppController(): AppControllerBuilder {
       return result;
     }
   }
-  return builder as AppControllerBuilder;
+  return builder as CommandDispatchBuilder;
 }
 
 
