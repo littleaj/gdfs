cb934319d2932eb7eb4428b5724c2fc55e185586
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 22:20:44 -0500

    login implementation

--- src/services/GoogleAuthService.ts
+++ src/services/GoogleAuthService.ts
@@ -1,12 +1,43 @@
-import { AuthService } from "./AuthService";
+import GoogleAuthConfig from "../model/GoogleAuthConfig";
+import AuthService from "./AuthService";
+
+type TokenClient = google.accounts.oauth2.TokenClient;
+type ClientConfigError = google.accounts.oauth2.ClientConfigError;
+type TokenResponse = google.accounts.oauth2.TokenResponse;
 
 export default class GoogleAuthService implements AuthService {
-  #client: GIS;
+  readonly #oauth2: GIS;
+  readonly #config: GoogleAuthConfig;
+  readonly #tokenClient: TokenClient;
+  /**
+   * space delimited list
+   */
+  private static readonly SCOPES: string = 
+    "https://www.googleapis.com/auth/drive.file";
 
-  constructor(client: GIS) {
-    this.#client = client;
+  constructor(oauth2: GIS, config: GoogleAuthConfig) {
+    this.#oauth2 = oauth2;
+    this.#config = config;
+    this.#tokenClient = this.#oauth2.initTokenClient({
+      client_id: this.#config.client_id,
+      error_callback: this.#onTokenError,
+      callback: this.#onTokenResponse,
+      scope: GoogleAuthService.SCOPES
+    });
   }
+
+  #onTokenError(error: ClientConfigError): void {
+    // TODO
+    console.error("Error response from token client: ", error);
+  }
+
+  #onTokenResponse(response: TokenResponse): void {
+    // TODO
+    console.log("Token response: ", response);
+  }
+
   login(): boolean {
+    this.#tokenClient.requestAccessToken();
     throw new Error("Method not implemented.");
   }
   logout(): boolean {
