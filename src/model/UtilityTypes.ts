bdeddcf5250a10f83712d377141888c2a13f4a8e
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-23 22:18:20 -0500

    more fun with generic types

--- src/model/UtilityTypes.ts
+++ src/model/UtilityTypes.ts
@@ -1,6 +1,14 @@
-export type PlainFunction<P extends any[] = any[], R = any> = (...args: P) => R;
+export type PlainFunction<Inputs extends any[] = any[], Output = any> =
+  (...args: Inputs) => Output;
 
 export type Maybe<T> = T | undefined;
 
-export type DefinedFunction<P extends any[] = any[], R = any> = R extends undefined ? never : PlainFunction<P, R>;
-export type TotalFunction<P extends any[] = any[], R = any> = R extends undefined | null ? never : PlainFunction<P, R>;
+export type DefinedFunction<Inputs extends any[] = any[], DefinedOutput = any> =
+  DefinedOutput extends undefined ? never 
+  : PlainFunction<Inputs, DefinedOutput>;
+
+export type TotalFunction<Inputs extends any[] = any[], DefinedNonnull = any> =
+  DefinedNonnull extends undefined | null ? never 
+  : PlainFunction<Inputs, DefinedNonnull>;
+
+export type Predicate<Inputs extends any[] = any[]> = PlainFunction<Inputs, boolean>;
