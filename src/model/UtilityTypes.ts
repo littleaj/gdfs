c2a14c2a7fb017b7f4f92f2081de6b736e66fa13
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-24 15:16:36 -0500

    remove useless util types

--- src/model/UtilityTypes.ts
+++ src/model/UtilityTypes.ts
@@ -1,14 +1,8 @@
 export type PlainFunction<Inputs extends any[] = any[], Output = any> =
   (...args: Inputs) => Output;
 
+// export type Nullable<T> = T | null;
+
 export type Maybe<T> = T | undefined;
-
-export type DefinedFunction<Inputs extends any[] = any[], DefinedOutput = any> =
-  DefinedOutput extends undefined ? never 
-  : PlainFunction<Inputs, DefinedOutput>;
-
-export type TotalFunction<Inputs extends any[] = any[], DefinedNonnull = any> =
-  DefinedNonnull extends undefined | null ? never 
-  : PlainFunction<Inputs, DefinedNonnull>;
 
 export type Predicate<Inputs extends any[] = any[]> = PlainFunction<Inputs, boolean>;
