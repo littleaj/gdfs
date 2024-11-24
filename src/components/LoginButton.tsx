93296977ed3c60a9845c11bf36a61c0e218384ff
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-24 15:14:47 -0500

    inject context into LoginButton

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,13 +1,17 @@
 import LoginTwoToneIcon from "@mui/icons-material/LoginTwoTone";
 import { Button } from "@mui/material";
 import { useButtonState } from "../model/contexts/UiStateContext";
-
-
-
-
-
+import { useContext } from "react";
+import { ApplciationCommands } from "../hooks/ApplicationCommands";
 
 export default function LoginButton() {
   const state = useButtonState("login");
-  return <Button variant="contained" color="primary" endIcon={<LoginTwoToneIcon />} disabled={state.disabled}>Login</Button>;
+  const commands = useContext(ApplciationCommands);
+
+  function doLogin() {
+    console.log("Attempting login...");
+    commands.login();
+  }
+
+  return <Button onClick={doLogin} variant="contained" color="primary" endIcon={<LoginTwoToneIcon />} disabled={state.disabled}>Login</Button>;
 };
