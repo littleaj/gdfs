034900dd5a130b413eae8d731eaab96d2bb80a7d
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-25 12:48:06 -0500

    added treeview with files + cleaned up login/populat logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,51 @@
+import { useColorScheme } from "@mui/material/styles";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import { Card, CardActions, CardContent, Typography } from "@mui/material";
 import useGoogleAuth from "./hooks/GoogleAuth";
 import useGoogleDrive from "./hooks/GoogleDrive";
+import { RichTreeView } from "@mui/x-tree-view";
+import { useEffect } from "react";
 
 function App() {
-  const [loggedIn, doLogin, doLogout ] = useGoogleAuth();
-  const [listFiles] = useGoogleDrive();
+  const { mode } = useColorScheme();
+  const [loggedIn, login, logout] = useGoogleAuth();
+  const [contents, refreshContents, resetContents] = useGoogleDrive();
+
+  useEffect(() => {
+    if (loggedIn) {
+      console.log("refreshing!");
+      refreshContents();
+    } else {
+      console.log("resetting");
+      resetContents();
+    }
+  }, [loggedIn, refreshContents, resetContents]);
+
+  if (!mode) {
+    return null;
+  }
+
   return (
     <>
       <header>
         <h1>GDFS</h1>
       </header>
       <Card variant="outlined">
-        <CardContent>
-          <Typography>This is where files go...</Typography>
-
-        </CardContent>
         <CardActions>
-          <LoginButton onClick={doLogin} disabled={loggedIn} />
-          <LogoutButton onClick={doLogout} disabled={!loggedIn} />
+          <LoginButton onClick={login} disabled={loggedIn} />
+          <LogoutButton onClick={logout} disabled={!loggedIn} />
         </CardActions>
+        <CardContent>
+
+          <Card variant="elevation">
+            <CardContent>
+              <Typography>This is where files go...</Typography>
+              <RichTreeView items={contents.map(f => ({ ...f, label: f.name }))} />
+            </CardContent>
+          </Card>
+        </CardContent>
       </Card>
     </>
   );
