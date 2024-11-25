2f4ccefd1cfd3caa04f80c66cf1edcdcebcf715b
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-25 15:03:06 -0500

    hooks naming convention (GoogleDrive)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import { Card, CardActions, CardContent, Typography } from "@mui/material";
 import useGoogleAuth from "./hooks/UseGoogleAuth";
-import useGoogleDrive from "./hooks/GoogleDrive";
+import useGoogleDrive from "./hooks/UseGoogleDrive";
 import { RichTreeView } from "@mui/x-tree-view";
 import { useEffect } from "react";
 
