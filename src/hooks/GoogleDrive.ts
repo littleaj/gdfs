803f725e7459d9f0b8a8e4f334a787d170b2e1d8
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-25 12:43:50 -0500

    add state variables for file list and refresh/reset functions.

--- src/hooks/GoogleDrive.ts
+++ src/hooks/GoogleDrive.ts
@@ -1,30 +1,37 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
-export default function useGoogleDrive() {
-  /**
-   * Print metadata for first 10 files.
-   */
-  const listFiles = useCallback(() => {
+// modifiedTime is RFC-3339
+type DriveFile = Required<Pick<gapi.client.drive.File, "id" | "name" | "modifiedTime" | "mimeType" | "parents" | "iconLink">> & {
+  "label": string
+};
+
+export default function useGoogleDrive(): [DriveFile[], () => void, () => void] {
+
+  const [driveContents, setDriveContents] = useState<DriveFile[]>([] as DriveFile[]);
+
+  const resetContents = useCallback(() => {
+    setDriveContents([]);
+  }, []);
+
+  const refreshContents = useCallback(() => {
     console.log("querying for files");
     gapi.client.drive.files.list({
-      pageSize: 10,
-      fields: "files(id, name)", // Q how does this work?
+      corpora: "user",
+      orderBy: "name_natural",
+      fields: "files(id, name, modifiedTime, mimeType, parents, iconLink)", // Q how does this work?
+      spaces: "drive"
     }).then((response) => {
       const files = response.result.files;
-      if (!files || files.length == 0) {
-        console.info("No files found");
-        return;
+      if (!files) {
+        console.warn("files refresh did not return a value...");
       }
-      // Flatten to string to display
-      const output = files.reduce(
-        (str, file) => `${str}${file.name} (${file.id})\n`,
-        "Files:\n");
-      console.info(output);
+      console.info(files?.length + "files found");
+      setDriveContents((files ?? []) as DriveFile[]);
     },
       (err) => {
         console.error("Error listing drive files: ", err);
       });
   }, []);
 
-  return [listFiles];
+  return [driveContents, refreshContents, resetContents];
 }
