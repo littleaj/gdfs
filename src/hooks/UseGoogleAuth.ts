7e6ec3c5a5f8c085e20d69157de00187ef528964
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-25 21:07:30 -0500

    move independent auth functions out of hook file

--- src/hooks/UseGoogleAuth.ts
+++ src/hooks/UseGoogleAuth.ts
@@ -1,91 +1,20 @@
 import { useEffect, useState } from "react";
 import { AuthService } from "../@types/AuthService";
-import { AuthConfig } from "../@types/AuthConfig";
 import _ from "lodash";
-import { TokenClient, TokenClientConfig, TokenResponse } from "../@types/gis";
-import { AUTH_CONFIG } from "./config";
-
-function createTokenClient(onTokenRequest: TokenClientConfig["callback"]): TokenClient {
-  console.log("Creating auth client...");
-  return google.accounts.oauth2.initTokenClient({
-    client_id: AUTH_CONFIG.client_id,
-    scope: AUTH_CONFIG.scopes,
-    callback: onTokenRequest,
-    error_callback: (err) => {
-      console.error("TC ClientConfigError: auth did not complete", err);
-    }
-  });
-}
-
-async function initializeApiClient(): Promise<void> {
-  return gapi.client.init({
-    clientId: AUTH_CONFIG.client_id,
-    discoveryDocs: [AUTH_CONFIG.discovery_doc],
-  });
-}
-
-async function loadGoogleApi(): Promise<void> {
-  return new Promise<void>((resolve) => {
-    gapi.load("client", () => resolve(initializeApiClient()));
-  });
-}
-
-function validateAuthConfig(...requiredKeys: (keyof AuthConfig)[]) {
-  requiredKeys.forEach(key => {
-    if (!AUTH_CONFIG[key]) {
-      throw new ReferenceError(`${key} is not defined`);
-    }
-  });
-}
-
-function handleTokenResponse(resp: TokenResponse): boolean {
-  const success: boolean = !!resp?.access_token;
-  console.log("Auth success: ", success);
-  if (!success) {
-    const err = {
-      code: resp.error ?? "UNKNOWN_ERROR",
-      description: resp.error_description ?? "...",
-      uri: resp.error_uri ?? "",
-    };
-    const errorMessage = `${err.code}: ${err.description} <${err.uri}>`;
-    console.error("TC callback error! ", errorMessage);
-    throw new Error(errorMessage);
-  }
-  return success;
-}
-
-function loginHandler(tokenClient: TokenClient) {
-  if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
-    console.log("Requesting consent...");
-    tokenClient.requestAccessToken({ prompt: "consent" });
-  } else {
-    console.log("auth: empty prompt...");
-    // Skip display of account chooser and consent dialog for an existing session.
-    tokenClient.requestAccessToken({ prompt: "" });
-  }
-}
-
-
-function logoutHandler() {
-  const token = gapi.client.getToken();
-  if (token) {
-    google.accounts.oauth2.revoke(token.access_token, () => {
-      console.log("session token revoked");
-    });
-    gapi.client.setToken(null);
-  }
-}
+import { createTokenClient, loadGoogleApis, loginHandler, logoutHandler } from "../model/GoogleAuthService";
+import { TokenClient } from "../@types/gis";
 
 export default function useGoogleAuth(): AuthService {
-  validateAuthConfig("client_id");
-
   const [loggedIn, setLoggedIn] = useState<boolean>(() => !!gapi?.client?.getToken()?.access_token);
 
-  const tokenClient = createTokenClient(_.flow(handleTokenResponse, setLoggedIn));
+  // using useState so it's only initialized once
+  const [tokenClient] = useState<TokenClient>(() => createTokenClient(() => setLoggedIn(true)));
   
-  const doLogin = _.partial(loginHandler, tokenClient);
+  const doLogin = () => {
+    _.partial(loginHandler, tokenClient)();
+    setLoggedIn(true);
+  };
+
   const doLogout = () => {
     logoutHandler();
     setLoggedIn(false);
@@ -94,7 +23,8 @@ export default function useGoogleAuth(): AuthService {
   // we only want this to load once
   useEffect(() => {
     console.info("Initializing auth and api clients...");
-    loadGoogleApi().then(() => {
+
+    loadGoogleApis().then(() => {
       console.info("gapi.client initialized");
     }, (err) => console.error("gapi.client init failed", err));
   }, []);
---
+++ src/model/GoogleAuthService.ts
@@ -0,0 +1,79 @@
+import { AuthConfig } from "../@types/AuthConfig";
+import { TokenClient, TokenResponse } from "../@types/gis";
+import { AUTH_CONFIG } from "../hooks/config";
+
+export function createTokenClient(callback: () => void): TokenClient {
+  validateAuthConfig("client_id");
+
+  console.log("Creating auth client...");
+  const client = google.accounts.oauth2.initTokenClient({
+    client_id: AUTH_CONFIG.client_id,
+    scope: AUTH_CONFIG.scopes,
+    callback: handleTokenResponse,
+    error_callback: (err) => {
+      console.error("TC ClientConfigError: auth did not complete", err);
+    }
+  });
+  callback();
+  return client;
+}
+
+async function initializeApiClient(): Promise<void> {
+  return gapi.client.init({
+    clientId: AUTH_CONFIG.client_id,
+    discoveryDocs: [AUTH_CONFIG.discovery_doc],
+  });
+}
+
+function validateAuthConfig(...requiredKeys: (keyof AuthConfig)[]) {
+  requiredKeys.forEach(key => {
+    if (!AUTH_CONFIG[key]) {
+      throw new ReferenceError(`${key} is not defined`);
+    }
+  });
+}
+
+function handleTokenResponse(resp: TokenResponse) {
+  const success: boolean = !!resp?.access_token;
+  console.log("Auth success: ", success);
+  if (!success) {
+    const err = {
+      code: resp.error ?? "UNKNOWN_ERROR",
+      description: resp.error_description ?? "...",
+      uri: resp.error_uri ?? "",
+    };
+    const errorMessage = `${err.code}: ${err.description} <${err.uri}>`;
+    console.error("TC callback error! ", errorMessage);
+    throw new Error(errorMessage);
+  }
+}
+
+export function loginHandler(tokenClient: TokenClient) {
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    console.log("Requesting consent...");
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    console.log("auth: empty prompt...");
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+export function logoutHandler() {
+  const token = gapi.client.getToken();
+  if (token) {
+    google.accounts.oauth2.revoke(token.access_token, () => {
+      console.log("session token revoked");
+    });
+    gapi.client.setToken(null);
+  }
+}
+
+export async function loadGoogleApis(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    gapi.load("client", () => resolve(initializeApiClient()));
+  });
+}
+
