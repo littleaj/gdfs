437293391734aa50d0ca66c7db57a26076d665ba
Arthur Little <1690572+littleaj@users.noreply.github.com>
2024-11-26 18:10:33 -0500

    fixed spacing

--- src/@types/gis.ts
+++ src/@types/gis.ts
@@ -1,3 +1,4 @@
-export type TokenClient = google.accounts.oauth2.TokenClient;export type TokenClientConfig = google.accounts.oauth2.TokenClientConfig;
+export type TokenClient = google.accounts.oauth2.TokenClient;
+export type TokenClientConfig = google.accounts.oauth2.TokenClientConfig;
 export type TokenResponse = google.accounts.oauth2.TokenResponse;
 
